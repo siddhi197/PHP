0859262a32ca7856376c5bc5d9e12d5c5f0020cc
Siddhi Kholkar <51523980+siddhi197@users.noreply.github.com>
2023-04-24 19:58:32 -0400

    Delete php directory

--- php/10_include_require/01_website_skeleton/about.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$companyName = 'TraversyMedia';
-?>
-
-<?php include "partials/header.php"; ?>
-<h1>About us</h1>
-<?php include "partials/footer.php"; ?>
--- php/10_include_require/02_functions/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// We need to have here require not include
-require_once 'math.php';
-
-echo add(4, 5).'<br>';
-echo subtract(9, 4);
